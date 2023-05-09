9e3432748ab5f8fa1cffc1a5f54f0cf7f33732c5
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-09 15:58:36 +0600

    layout

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+export default function Layout({ children }) {
+  return (
+    <>
+      <div className="flex">
+        <div className="w-1/5">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde iusto
+          tenetur pariatur eligendi aperiam dicta natus quidem dolor ad at,
+          consequuntur excepturi quia fugit expedita nesciunt quae placeat iure
+          corporis.
+        </div>
+        <div className="w-4/5">{children}</div>
+      </div>
+    </>
+  );
+}
---
+++ pages/dashboard.js
@@ -0,0 +1,7 @@
+import Layout from "@/components/Layout";
+
+export default function dashboard() {
+  return (
+    <Layout>dashboard</Layout>
+  )
+}
