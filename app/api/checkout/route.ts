31634350f0caabd4bd80c2b1a06ee9bf9abc25c7
mabuhanifa <moh.abuhanifa@gmail.com>
2025-07-15 05:47:06 +0600

    lib updated

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,11 +1,6 @@
 /* app/api/checkout/route.ts */
+import { stripe } from "@/lib/stripe";
 import { NextRequest, NextResponse } from "next/server";
-import Stripe from "stripe";
-
-// Initialize Stripe with secret key
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
-});
 
 export async function POST(request: NextRequest) {
   try {
